aa99c7fa5cefd71f5dc5c5e76beab37e399893a7
Wolfloh <yota4glt@yandex.ru>
2023-09-09 15:57:01 +0300

    adding via the store

--- src/components/UI/Products/Products.jsx
+++ src/components/UI/Products/Products.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { productStore } from '../../../stores/ProductStore'
 import cl from './Products.module.scss'
 import { observer } from "mobx-react-lite";
 import { Loader } from '../Loader/Loader';
 import { ComponentError } from '../ComponentError/ComponentError.jsx';
 import { Link } from 'react-router-dom';
+import { basket } from '../../../stores/BasketStore';
+import green_mark from './green_mark.png'
 
 
 export const Products = observer(() => {
     const { products, isLoading, error, category, setCategory } = productStore;
+    const { basketList, addToCart, removeFromTheBasket } = basket;
+    const [linkState, setLinkState] = useState(``)
     useEffect(() => { setCategory(category) }, []);
     const getRating = (rate) => {
         if (rate > 4) {
@@ -25,11 +29,40 @@ export const Products = observer(() => {
         return <ComponentError />
     }
 
-
+    let markForClick = true;
+    let flag;
     return (
         <div className={cl.products}>
             {products.map(product =>
-                <Link to={`${product.id}`} key={product.id} className={cl.products__item}>
+                <Link
+                    onMouseDown={() => {
+                        markForClick = true;
+                        flag = true;
+                        setTimeout(() => {
+                            if (markForClick) {
+                                const added = basketList.filter(prod => prod.id === product.id).length;
+                                if (added) {
+                                    removeFromTheBasket(product.id)
+                                    flag = false;
+                                } else {
+                                    addToCart(product)
+                                }
+                                markForClick = false;
+                            }
+                        }, 300)
+                    }}
+                    onMouseUp={() => {
+                        if (!flag) {
+                            markForClick = false;
+                        }
+                        if (markForClick) {
+                            setLinkState(`${product.id}`)
+                        }
+                        markForClick = false;
+                    }}
+                    to={linkState}
+                    key={product.id}
+                    className={cl.products__item}>
                     <div className={cl.products__img_wrapper}>
                         <img className={cl.products__img} src={product.image} alt={product.title} />
                     </div>
@@ -40,6 +73,14 @@ export const Products = observer(() => {
                         </h5>
                     </div>
                     <div className={cl.products__price}>{product.price}$</div>
+                    <div className={cl.products__add}>
+                        {basketList.filter(prod => prod.id === product.id).length
+                            ?
+                            <img className={cl.products__add_mark} src={green_mark} alt="green_mark" />
+                            :
+                            <div className={cl.products__add_text}>Удерживайте для добавления в корзину</div>
+                        }
+                    </div>
                 </Link>
             )}
         </div>
